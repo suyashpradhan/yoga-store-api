f0cbf592431f8ac2a530c3fd3e5330fb0fe48a64
suyashpradhan <pradhansuyash39@gmail.com>
2021-07-02 01:47:30 +0530

    fix: minor bug fixes

--- routes/register.route.js
+++ routes/register.route.js
@@ -8,22 +8,20 @@ router.route("/").post(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   try {
-    const savedUser = await User.create({
+    const user = await User.create({
       firstName,
       lastName,
       email,
       password,
     });
-    let userWishlist = await Wishlist({ userId: savedUser._id, products: [] });
-    userWishlist = await userWishlist.save();
+    const savedUser = await user.save();
     res.status(201).json({
       success: true,
       message: "Succesfully signed up.",
       _id: savedUser._id,
     });
   } catch (error) {
-    const errors = errorHandler(error);
-    res.status(503).json({ errors });
+    res.status(503).json({ success: false, message: error.message });
   }
 });
 
